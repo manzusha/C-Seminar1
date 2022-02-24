3790c77fd44c14c6369e2591967b8c03dc155d2d
Manzusha <manis_15club@mail.ru>
2022-02-25 02:07:06 +0500

    Add exercise 3

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -79,3 +79,16 @@ if((c>a) && (c>b))
 */
 
 
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("Четное число");
+    }
+
+else
+
+{
+    Console.WriteLine("Нечетное число");
+}
