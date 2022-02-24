dff5ff4d04015e9e25888845edf15150fd48dbd5
Manzusha <manis_15club@mail.ru>
2022-02-25 00:50:23 +0500

    Add 2 exersise

---
+++ Seminar1/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+//Задача №1
+
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+Console.WriteLine("Число a большее");
+Console.WriteLine("Число b меньшее");
+}
+
+else
+{
+Console.WriteLine("Число b большее");
+Console.WriteLine("Число a меньшее");
+}
+*/
+
+/*
+//Задача №2
+
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число с: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a>b) 
+{
+    if (a>c)
+    {
+    Console.WriteLine("Максимальное число a");
+    }
+}
+if (b>a)
+{
+    if (b>c)
+    {   
+    Console.WriteLine("Максимальное число b");
+    }
+}
+
+if (c>a)
+{
+    if (c>b)
+    {   
+    Console.WriteLine("Максимальное число c");
+    }
+}
+
+*/
+
+/*
+//Алтернативное решение задачи №2
+
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число с: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if((a>b) && (a>c))
+{   
+    Console.WriteLine("Максимальное число a");
+    }
+if((b>a) && (b>c))
+{   
+    Console.WriteLine("Максимальное число b");
+    }
+if((c>a) && (c>b))
+{   
+    Console.WriteLine("Максимальное число c");
+    }
+*/
+
+
