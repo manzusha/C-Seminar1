4800cb4887f4374a8eda3bbcdd936394ba0e2343
Manzusha <manis_15club@mail.ru>
2022-03-06 21:45:48 +0500

    Added homework from Seminar 2

---
+++ Seminar2/Program.cs
@@ -0,0 +1,124 @@
+﻿/* ЗАДАЧА 10
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5; 782 -> 8; 918 -> 1
+*/
+
+//Console.Write("Введите число: ");
+//int N = Convert.ToInt32(Console.ReadLine());
+
+
+/*
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int n = num / 10 % 10;
+Console.WriteLine("Второе число " + n);
+*/
+
+// Задача 10. Найти вторую цифру трехзначного числа
+/*
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void FindNumb(int num)
+{
+    if(num >= 100 && num < 1000)
+    {
+        Console.WriteLine("Вторая цифра " + num / 10 % 10);
+        }
+
+    else
+    {
+        Console.WriteLine("Введите трехзначное число");
+        }
+}
+
+FindNumb(num);
+*/
+
+//Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/*
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void FindNumb(int num)
+{
+    if(num < 100)
+    {
+        Console.WriteLine("Третье цифры нет");
+        }
+
+    else {
+        while(num >=1000) {
+            num /= 10;
+        }
+
+        Console.WriteLine("Третья цифра " + num % 10);
+        }
+            
+    
+}
+
+FindNumb(num);
+*/
+
+
+//Задача 15. 
+/*Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int DayOff(int num)
+
+{
+    if (num == 1)
+    {
+        Console.WriteLine("Понедельник");
+        return num;
+    }
+
+    if (num == 2)
+    {
+        Console.WriteLine("Вторник");
+        return num;
+    }
+
+    if (num == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+
+    if (num == 4)
+    {
+        Console.WriteLine("Четверг");
+    }
+
+    if (num == 5)
+    {
+        Console.WriteLine("Пятница");
+    }
+
+    if (num == 6)
+    {
+        Console.WriteLine("Суббота");
+    }
+
+    if (num == 7)
+    {
+        Console.WriteLine("Воскресенье");
+    }
+    return num;
+}
+
+Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num <= 7 && num >= 1)
+{
+    DayOff(num);
+}
+
+else
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
+*/
